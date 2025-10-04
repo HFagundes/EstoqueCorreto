d397d6e5bfad0f8d6b72be0a53b0e39cb531d87b
Hitler Fagundes <111394175+HFagundes@users.noreply.github.com>
2025-10-04 12:48:45 -0300

    Update ConexaoBD.cs

--- Controle de estoque/Banco de dados/ConexaoBD.cs
+++ Controle de estoque/Banco de dados/ConexaoBD.cs
@@ -2,7 +2,7 @@ using MySql.Data.MySqlClient;
 
 public class ConexaoBD
 {
-    private string connectionString = "Server=localhost;Database=estoque_db;Uid=root;Pwd=suasenha;";
+    private string connectionString = "Server=localhost;Database=estoque_db;Uid=root;Pwd=123;";
 
     public MySqlConnection ObterConexao()
     {
